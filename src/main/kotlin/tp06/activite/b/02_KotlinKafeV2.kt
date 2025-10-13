03a7281cf3efe6445264c98642a36ebf9c5c2e4f
melinedallalba <meline.dallalba@etu.iut-tlse3.fr>
2025-10-13 16:36:37 +0200

    fix #2 Programme Kotlin Kafe v2 commenté.

--- src/main/kotlin/tp06/activite/b/02_KotlinKafeV2.kt
+++ src/main/kotlin/tp06/activite/b/02_KotlinKafeV2.kt
@@ -1,7 +1,10 @@
 package tp06.activite.b
 
 // Quel est le but du programme principal ?
-// ...
+// Créer un tableau avec différentes valeurs correspondant à des boissons (expresso, Latte, etc.)
+// Afficher le résultat de ce tableau
+// Puis remplacer Latte par Frappé
+// Et enfin afficher le résultat suite à la modification
 
 fun main() {
     println("=== Bienvenue au Kotlin Café ===")
@@ -11,7 +14,8 @@ fun main() {
     afficherCarte(boissons)
     // Le programme modifie bien la carte des boissons alors que la variable boissons est
     // déclarée avec le mot clé "val". Peux-tu expliquer pourquoi cela est possible ?
-    // ...
+    // Le tableau est certes immuable, mais les variables à l'intérieur peuvent référer d'autres objets.
+    // et la variable val est définie par arrayOf
 }
 
 fun modifieCarte(boissons: Array<String>, numeroBoisson: Int, nouvelleBoisson: String) {
