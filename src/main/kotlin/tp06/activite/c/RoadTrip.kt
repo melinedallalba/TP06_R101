849ed49ecd2282365e057a9e55ae960b12a75655
melinedallalba <meline.dallalba@etu.iut-tlse3.fr>
2025-10-13 16:55:51 +0200

    fix #3 Programme Road Trip commenté.

--- src/main/kotlin/tp06/activite/c/RoadTrip.kt
+++ src/main/kotlin/tp06/activite/c/RoadTrip.kt
@@ -1,16 +1,20 @@
 package tp06.activite.c
 
 fun main() {
+    //  Le titre et la création du tableau : laissés dans fun main()
     println("=== Organisation du Road Trip ===")
 
     val etapes = arrayOf("Paris", "Lyon", "Marseille", "Nice")
 
+    // Afficher les étapes initiales entre Paris et Nice : laissé dans fun main()
     println("Étapes initiales :")
     for (i in 0 until etapes.size) {
         println(" - Étape ${i + 1} : ${etapes[i]}")
     }
 
     // Modification : changement d'avis sur une destination
+    // Fonction 1 : Modifier une des valeurs du tableau et la remplacer
+    // Signature première fonction : fun modifierEtapes(etapes : array<String>) : String
     println("\nChangement de programme : on remplace Marseille par Montpellier...")
     for (i in 0 until etapes.size) {
         if (etapes[i] == "Marseille") {
@@ -19,6 +23,8 @@ fun main() {
     }
 
     // Supposons que Nice soit trop chère → on la marque comme "Annulée"
+    // Fonction 2 : Annuler une des étapes
+    // Signature deuxième fonction : fun annulerEtapes(etapes : array<String>) : String
     println("Finalement, Nice est trop chère. On annule cette étape.")
     for (i in 0 until etapes.size) {
         if (etapes[i] == "Nice") {
@@ -26,7 +32,7 @@ fun main() {
         }
     }
 
-    // Affichage final
+    // Affichage final suite à toutes les modifications : laissé dans fun main ()
     println("\nItinéraire mis à jour :")
     for (i in 0 until etapes.size) {
         println(" - Étape ${i + 1} : ${etapes[i]}")
@@ -35,5 +41,5 @@ fun main() {
     println("\nRoad trip prêt ! Il ne reste plus qu’à faire les valises 🎒")
 }
 
-// Signature première fonction : .................................
-// Signature deuxième fonction : .................................
+
+
