6a6c36c34d1eb22589e509a7a00f6e20e1b60632
melinedallalba <meline.dallalba@etu.iut-tlse3.fr>
2025-10-14 08:25:09 +0200

    fix #4 Programme RoadTrip amélioré.

--- src/main/kotlin/tp06/activite/c/RoadTrip.kt
+++ src/main/kotlin/tp06/activite/c/RoadTrip.kt
@@ -1,5 +1,8 @@
 package tp06.activite.c
 
+// import kotlin.text.get
+// import kotlin.text.set
+
 fun main() {
     //  Le titre et la création du tableau : laissés dans fun main()
     println("=== Organisation du Road Trip ===")
@@ -12,34 +15,37 @@ fun main() {
         println(" - Étape ${i + 1} : ${etapes[i]}")
     }
 
+    modifierEtapes(etapes, numeroEtape = 2, nouvelleEtape = "Montpellier")
+    annulerEtapes(etapes)
+    afficherRoadTripFinal(etapes)
+
+}
+
     // Modification : changement d'avis sur une destination
     // Fonction 1 : Modifier une des valeurs du tableau et la remplacer
-    // Signature première fonction : fun modifierEtapes(etapes : array<String>) : String
-    println("\nChangement de programme : on remplace Marseille par Montpellier...")
-    for (i in 0 until etapes.size) {
-        if (etapes[i] == "Marseille") {
-            etapes[i] = "Montpellier"
-        }
+    // Signature première fonction : fun modifierEtapes(etapes : Array<String>)
+    fun modifierEtapes(etapes: Array<String>, numeroEtape: Int, nouvelleEtape: String) {
+        println("\nChangement de programme : on remplace Marseille par Montpellier...")
+        etapes[numeroEtape] = nouvelleEtape
     }
 
     // Supposons que Nice soit trop chère → on la marque comme "Annulée"
     // Fonction 2 : Annuler une des étapes
-    // Signature deuxième fonction : fun annulerEtapes(etapes : array<String>) : String
+    // Signature deuxième fonction : fun annulerEtapes(etapes : array<String>)
+fun annulerEtapes(etapes: Array<String>) {
     println("Finalement, Nice est trop chère. On annule cette étape.")
     for (i in 0 until etapes.size) {
         if (etapes[i] == "Nice") {
             etapes[i] = "(Annulé) Nice"
         }
     }
+}
 
+fun afficherRoadTripFinal(etapes: Array<String>) {
     // Affichage final suite à toutes les modifications : laissé dans fun main ()
     println("\nItinéraire mis à jour :")
     for (i in 0 until etapes.size) {
         println(" - Étape ${i + 1} : ${etapes[i]}")
     }
-
     println("\nRoad trip prêt ! Il ne reste plus qu’à faire les valises 🎒")
 }
-
-
-
